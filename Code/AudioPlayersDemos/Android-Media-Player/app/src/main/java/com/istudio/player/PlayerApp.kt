9317d90f6da9b5505d7b29c5e7f6bb9aaea74ab7
Devrath <devrath.dev595@gmail.com>
2024-02-23 20:41:45 +0530

    added methods

--- Code/AudioPlayersDemos/Android-Media-Player/app/src/main/java/com/istudio/player/PlayerApp.kt
+++ Code/AudioPlayersDemos/Android-Media-Player/app/src/main/java/com/istudio/player/PlayerApp.kt
@@ -2,5 +2,5 @@ package com.istudio.player
 
 import android.app.Application
 
-const val APP_TAG = "Player"
+const val APP_TAG = "Pokemon"
 class PlayerApp : Application()
--- Code/AudioPlayersDemos/Android-Media-Player/app/src/main/java/com/istudio/player/service/PlayerService.kt
+++ Code/AudioPlayersDemos/Android-Media-Player/app/src/main/java/com/istudio/player/service/PlayerService.kt
@@ -1,19 +1,72 @@
 package com.istudio.player.service
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
+import android.util.Log
+import com.istudio.player.APP_TAG
+import com.istudio.player.R
 
 class PlayerService : Service() {
 
+    private lateinit var player : MediaPlayer
+    private val context : Context by lazy { this }
+
+    /**
+     * < ************************** > Service Methods < **************************>
+     */
+
+    override fun onCreate() {
+        initOnCreate()
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+        return initOnBind()
     }
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        initOnStartCommand()
         return super.onStartCommand(intent, flags, startId)
     }
 
+
+    override fun onDestroy() {
+        initOnDestroy()
+        super.onDestroy()
+    }
+    /**
+     * < ************************** > Service Methods < **************************>
+     */
+
+
+
+    /**
+     * < ************************** > Init Methods < *****************************>
+     */
+
+    private fun initOnCreate() {
+        Log.d(APP_TAG, "PlayerService - onCreate is called")
+        // Initialize the media player
+        player = MediaPlayer.create(context,R.raw.demo_music)
+    }
+
+    private fun initOnBind(): IBinder? {
+        Log.d(APP_TAG, "PlayerService - onBind is called")
+        return null
+    }
+
+    private fun initOnStartCommand() {
+        Log.d(APP_TAG, "PlayerService - onStartCommand is called")
+    }
+
+    private fun initOnDestroy() {
+        Log.d(APP_TAG, "PlayerService - onDestroy is called")
+    }
+    /**
+     * < ************************** > Init Methods < *****************************>
+     */
 
 }
