ffd2a10b0e89b464fd247cbaffb770674be59132
DevIndiGG <devrath.ad@indi.gg>
2024-02-23 13:13:37 +0530

    added

---
+++ Code/AudioPlayersDemos/Android-Media-Player/app/src/main/java/com/istudio/player/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.istudio.player
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.istudio.player.ui.theme.PlayerTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
